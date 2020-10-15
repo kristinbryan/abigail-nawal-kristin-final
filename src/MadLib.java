dbe9fe4fc01ade5b7aa1d8c14d5599ffa3d9f797
kristinbryan <kbryan@macalester.edu>
2020-10-15 14:24:51 -0500

    no text files

--- src/MadLib.java
+++ src/MadLib.java
@@ -2,14 +2,35 @@ import java.util.List;
 
 public class MadLib {
     public List<GameElement> gameElements;
+    public FixedText fixedText;
+    public Substitution substitution;
 
     public MadLib(List<GameElement> gameElements) {
         System.out.println(madLib());
     }
 
     public String madLib(){
-        List<GameElement> fixedText = FixedText;
+        List<GameElement> madLib = new List<GameElement> { FixedText"worlds smartest", Substitution "[noun]", FixedText "GOES TO",  Substitution "[place]"};
 
-        return getText
+        return getText;
     }
+
+
+public static void main(String[] args) {
+    //fixedText
+    //
+    // List<GameElements> Madlib = [ FixedText "WORLD'S SMARTEST", Substitution "[noun]", FixedText "GOES TO",  Substitution "[place]"]
+    //
+    //for i in madlib if i == subsitution, repalce with input
+    //
+    //
+    // List <Strings> userInput = [] when the user inputs a string in the console, it is put in this list
+    //
+    //
+
+
+
+}
+
+
 }
